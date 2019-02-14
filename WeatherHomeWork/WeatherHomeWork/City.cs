4ce9aa3b5d5569677397a67b7696be7536e377e9
Bharmaglot <Valder1984@mail.ru>
2019-02-14 18:30:11 +0700

    Задание вышлю в субботу. Пока заготовка к заданию "Создать приложение, показанное на уроке". Владимир Болбат

---
+++ WeatherHomeWork/WeatherHomeWork/City.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherHomeWork
+{
+    class City
+    {
+        public string CityName { get; set; }
+
+        public int TemperatureMax { get; set; }
+
+        public int TemperatureMin { get; set; }
+
+        public int PressureMax { get; set; }
+
+        public int PressureMin { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.CityName,10}";
+        }
+
+        internal string Print()
+        {
+            return $" {this.CityName,10} {TemperatureMin,3} - {TemperatureMax,3} °c {PressureMin} - {PressureMax} мм.рт.ст.";
+        }
+    }
+}
